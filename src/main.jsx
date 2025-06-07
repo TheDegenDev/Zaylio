3a5617225bbcea46be63d89632f4e7dc0b2a0aaa
TheDegenDev <freezybam@gmail.com>
2025-06-08 01:26:37 +0200

    add PWA

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,12 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    
   </StrictMode>,
 )
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => console.log('✅ Service Worker enregistré'))
+    .catch((err) => console.error('❌ Erreur Service Worker:', err));
+}
