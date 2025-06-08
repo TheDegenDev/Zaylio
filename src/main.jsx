bdc2757014141ac0528ce5755b00d934aab67c25
TheDegenDev <freezybam@gmail.com>
2025-06-08 02:02:09 +0200

    Fix Problem Logo Flash

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Enregistrement du Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => {
+      console.log('✅ Service Worker enregistré')
+      // Une fois le Service Worker chargé, on affiche l'application
+      document.getElementById('root').classList.add('loaded')
+    })
+    .catch((err) => console.error('❌ Erreur Service Worker:', err));
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    
   </StrictMode>,
 )
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(() => console.log('✅ Service Worker enregistré'))
-    .catch((err) => console.error('❌ Erreur Service Worker:', err));
-}
